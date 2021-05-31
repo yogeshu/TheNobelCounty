493c8f2337096402e8e0bd10fca71f8fedad1e43
yogeshu <yogeshbhavsar1994@gmail.com>
2021-05-31 14:37:38 +0530

    The Section of Live

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React, { useState, useEffect,useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Navbar from "./navbar";
 
 import Header from "./Header";
-import TheNobelCard from './TheNobelCard'
-import PrivacyCookie from './privacycookie'
-import TheIframe from './theiframe';
+import TheNobelCard from "./TheNobelCard";
+import PrivacyCookie from "./privacycookie";
+import TheIframe from "./theiframe";
 
-import Footer from './footer'
+import Footer from "./footer";
 function App() {
   const [data, setData] = useState([]);
-  const [currentResult,setCurrentResult] = useState(0);
-  const [show , setShow] = useState(false)
+  const [currentResult, setCurrentResult] = useState(0);
+  const [show, setShow] = useState(false);
   const alert = document.getElementsByClassName('priavcy-cookie"');
-  const setCard = ()=>{
+  const setCard = () => {
     // if(show === false){
     //   alert.classList.add('show');
     // }
-  }
+  };
   setCard();
   const Nobellist = () => {
     try {
@@ -32,35 +32,28 @@ function App() {
     Nobellist();
   }, []);
   console.log(data);
- const per_page = 10;
- const offset = currentResult * per_page;
- const currentPageData = data.slice(offset,offset + 10)
+  const per_page = 10;
+  const offset = currentResult * per_page;
+  const currentPageData = data.slice(offset, offset + 10);
 
   return (
     <div className="container-fluid">
       <Navbar />
       <Header />
-      <div className="container-xxl bg-dark text-center" >
-              <div className="row">
-                <div className="col-12" >
-      {data !== null
-        ? currentPageData.map((country, index) => {
-             
-            return (
-              
-              
-              
-              <TheNobelCard country={country} index={index}/>
-            
-              );
-          })
-        : "loding"}
-</div>
-              </div>
-              </div>
-        <TheIframe/>
-        <PrivacyCookie/>
-        <Footer/>
+      <div className="container-xxl bg-dark text-center">
+        <div className="row">
+          <div className="col-12">
+            {data !== null
+              ? currentPageData.map((country, index) => {
+                  return <TheNobelCard country={country} index={index} />;
+                })
+              : "loding"}
+          </div>
+        </div>
+      </div>
+      <TheIframe />
+      <PrivacyCookie />
+      <Footer />
     </div>
   );
 }
