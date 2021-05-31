9cb3d60bff718ca401fa58ebddf91df753929ae3
yogeshu <yogeshbhavsar1994@gmail.com>
2021-05-31 15:43:47 +0530

    The nav element gap in navbar component page

--- src/navbar.js
+++ src/navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = ()=> {
     return (
 
-<nav className="navbar navbar-expand-lg navbar-dark bg-dark py-5">
+<nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
   <a className="navbar-brand" href="#">Navbar</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
